c8fe7fe7f62feed68ef0aef2a633fa489e8f4316
MRSa <mrsa@myad.jp>
2021-09-12 23:12:59 +0900

    WPZのフォーカスとシャッター機能の調整途中。

--- app/src/main/java/jp/osdn/gokigen/gokigenassets/camera/vendor/pixpro/wrapper/command/messages/specific/PixproExecuteShutter.kt
+++ app/src/main/java/jp/osdn/gokigen/gokigenassets/camera/vendor/pixpro/wrapper/command/messages/specific/PixproExecuteShutter.kt
@@ -4,12 +4,13 @@ import jp.osdn.gokigen.gokigenassets.camera.vendor.pixpro.wrapper.command.messag
 import jp.osdn.gokigen.gokigenassets.camera.vendor.pixpro.wrapper.command.messages.base.IPixproMessages
 import jp.osdn.gokigen.gokigenassets.camera.vendor.pixpro.wrapper.command.messages.base.PixproCommandBase
 
-class PixproExecuteShutter(private val callback: IPixproCommandCallback, posX: Int = 0x00004dbf.toInt(), posY: Int = 0xb6658801.toInt()) : PixproCommandBase()
+class PixproExecuteShutter(private val callback: IPixproCommandCallback, posX: Int = 0x00004dbf, posY: Int = 0xb6658801.toInt()) : PixproCommandBase()
 {
     private val data00: Byte = (0x000000ff and posX).toByte()
     private val data01: Byte = (0x0000ff00 and posX shr 8).toByte()
     private val data02: Byte = (0x00ff0000 and posX shr 16).toByte()
     private val data03: Byte = (-0x1000000 and posX shr 24).toByte()
+
     private val data10: Byte = (0x000000ff and posY).toByte()
     private val data11: Byte = (0x0000ff00 and posY shr 8).toByte()
     private val data12: Byte = (0x00ff0000 and posY shr 16).toByte()
@@ -19,9 +20,12 @@ class PixproExecuteShutter(private val callback: IPixproCommandCallback, posX: I
     {
         return (IPixproMessages.SEQ_SHUTTER)
     }
+
     override fun commandBody(): ByteArray
     {
-        return byteArrayOf( //  (byte) 0xf9, (byte) 0x03
+        return byteArrayOf(
+/*
+            //  (byte) 0xf9, (byte) 0x03
             0x2e.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
             0x30.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
             0xf9.toByte(), 0x03.toByte(), 0x00.toByte(), 0x00.toByte(),
@@ -68,13 +72,13 @@ class PixproExecuteShutter(private val callback: IPixproCommandCallback, posX: I
             0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
 
             0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
-            0xbf.toByte(), 0x4d.toByte(), 0x00.toByte(), 0x00.toByte(),  //
+            data00, data01, data02, data03,  //
             0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
-            0x01.toByte(), 0x88.toByte(), 0x65.toByte(), 0xb6.toByte(),  //
+            data10, data11, data12, data13,  //
 
             0xff.toByte(), 0xff.toByte(), 0xff.toByte(), 0xff.toByte(),
             0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
-
+*/
 
             // (byte) 0xef, (byte) 0x03 [96 bytes + 8bytes]
             0x2e.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
